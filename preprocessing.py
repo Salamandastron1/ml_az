611ff823c67f1f9093daf69a7805b8e93dff6b6f
Salamandastron1 <tag.garrity@gmail.com>
2021-12-29 16:45:30 -0500

    train and test sets, encode columns as nums

--- preprocessing.py
+++ preprocessing.py
@@ -4,6 +4,8 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split   
 
 def preProcess(dataset):
     x = dataset.iloc[:, :-1].values
@@ -16,7 +18,15 @@ def preProcess(dataset):
     x[:, 1:3] = imputer.transform(x[:, 1:3])
     ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
     x = np.array(ct.fit_transform(x))
-    print(x)
+    le = LabelEncoder()
+    y = le.fit_transform(y)
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+
+    print(x_train)
+    print(x_test)
+    print(y_train)
+    print(y_test)
 
 def main():
     dataset = pd.read_csv('Data.csv')
